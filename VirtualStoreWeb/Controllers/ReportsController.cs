0b61748da582e8309c6e973afe7eaa1eb36a329f
KorbenKross <31703739+KorbenKross@users.noreply.github.com>
2018-06-09 01:02:48 +0400

    Update ReportsController.cs

--- VirtualStoreWeb/Controllers/ReportsController.cs
+++ VirtualStoreWeb/Controllers/ReportsController.cs
@@ -116,29 +116,6 @@ namespace VirtualStoreWeb.Controllers
                 FileName = Server.MapPath("~/ReportExcle")
             });
 
-            //byte[] byteArray = File.ReadAllBytes(Server.MapPath("~/ReportExcle"));
-            //using (MemoryStream memoryStream = new MemoryStream())
-            //{
-            //    memoryStream.Write(byteArray, 0, byteArray.Length);
-            //    using (WordprocessingDocument doc = WordprocessingDocument.Open(memoryStream, true))
-            //    {
-            //        HtmlConverterSettings settings = new HtmlConverterSettings()
-            //        {
-            //            PageTitle = "My Page Title"
-            //        };
-            //        XElement html = HtmlConverter.ConvertToHtml(doc, settings);
-
-            //        // Note: the XHTML returned by ConvertToHtmlTransform contains objects of type
-            //        // XEntity. PtOpenXmlUtil.cs defines the XEntity class. See
-            //        // [url]http://blogs.msdn.com/ericwhite/archive/2010/01/21/writing-entity-references-using-linq-to-xml.aspx[/url]
-            //        // for detailed explanation.
-            //        //
-            //        // If you further transform the XML tree returned by ConvertToHtmlTransform, you
-            //        // must do it correctly, or entities do not serialize properly.
-
-            //        File.WriteAllText("Test.html", html.ToStringNewLineOnAttributes());
-            //    }
-            //}
 
                 return RedirectPermanent("/Reports/Reports");
         }
